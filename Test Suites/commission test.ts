ab1fa2e45911ec0d97f92d18161aded1eeb87404
rkulkarn <rkulkarn@41XHMV1-E6330.callidussoftware.com>
2018-09-04 16:21:25 +0530

    Added New test case Plan Components, Credit Types

--- Test Suites/commission test.ts
+++ Test Suites/commission test.ts
@@ -4,7 +4,7 @@
    <name>commission test</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-09-04T14:40:56</lastRun>
+   <lastRun>2018-09-04T15:34:43</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
@@ -24,33 +24,21 @@
       <testCaseId>Test Cases/Earning Groups exist in the system</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>863ff84a-b1b2-4f77-b945-2d58de396f1f</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/Commission Logout</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
       <guid>e095c799-d9a4-4d81-87ce-17aa360cdb6e</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Commission Login</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>dd578d47-7a80-4786-8450-046f891c0f0e</guid>
+      <guid>271f1857-f10b-44d5-904e-5969e703cd6d</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/EventTypes exist in the System</testCaseId>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Commission Logout</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>162bd592-0a7d-4ac7-85e0-e3f0545b3c61</guid>
+      <guid>c835be74-c341-4613-96be-fba38b42db9b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Position Groups exist in the system</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>fc14f315-b08a-403a-8020-6174c400be8a</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/Fixed ValueTypes exist in the system</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
