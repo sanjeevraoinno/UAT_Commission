d0c20e69685532d7f36ad61574846f59088b543d
sanrao <sanrao@1971WS1-E6330.callidussoftware.com>
2018-09-05 16:22:12 +0530

    Updated Login/Logout with data driven

--- Scripts/Commission Login/Script1536051665039.groovy
+++ Scripts/Commission Login/Script1536051665039.groovy
@@ -2,6 +2,7 @@ import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import org.apache.http.auth.UsernamePasswordCredentials as UsernamePasswordCredentials
 import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
@@ -21,13 +22,13 @@ import internal.GlobalVariable as GlobalVariable
 
 WebUI.openBrowser('')
 
-WebUI.navigateToUrl('https://biz3-tst.callidusondemand.com/SalesPortal/#!/')
+WebUI.navigateToUrl(CallidusURL)
 
 WebUI.maximizeWindow()
 
-WebUI.setText(findTestObject('Page_Sales Performance Home/input_username'), 'rkulkarn@calliduscloud.com')
+WebUI.setText(findTestObject('Page_Sales Performance Home/input_username'), User)
 
-WebUI.setEncryptedText(findTestObject('Page_Sales Performance Home/input_password'), 'WcIEjFhlsgB1+K0jmcpkSg==')
+WebUI.setText(findTestObject('Page_Sales Performance Home/input_password'), Password)
 
 WebUI.click(findTestObject('Page_Sales Performance Home/button_Login'))
 
---
+++ Test Suites/TestDemo.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestDemo</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-09-05T15:29:48</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>9f09e11b-e4a2-42d2-b292-7800666e5ffd</testSuiteGuid>
+   <testCaseLink>
+      <guid>409a821a-3538-459f-9393-39dd45d12e69</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Earning Codes exist in the system</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
